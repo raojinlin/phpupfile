a64ab62f430563264cc2d4db78d5753672a9ab90
raojinlin <1239015423@qq.com>
2021-10-16 14:52:51 +0800

    Add charset & Access-control header & remove schema charset

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return [
   'database' => [
       'host' => '127.0.0.1',
       'port' => 3306,
-      'user' => 'root',
-      'password' => 'password',
+      'user' => 'upfile',
+      'password' => 'MyPassword',
       'dbname' => 'mydb'
   ],
   'upload' => [
--- photos_api.php
+++ photos_api.php
@@ -6,6 +6,8 @@ require_once __DIR__ . '/createDb.php';
 $photos = new Photos(createDbConnection());
 
 header('content-type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: *');
 
 echo json_encode($photos->getPhotos());
 
